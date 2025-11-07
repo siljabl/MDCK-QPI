35e625adbcecad309e5393c226fb555ac9712a1e
silja uio <s.b.lastad@mn.uio.no>
2025-11-07 16:13:59 +0100

    adding zero-correction

--- code/preprocessing/transform_3D_to_2D.py
+++ code/preprocessing/transform_3D_to_2D.py
@@ -13,6 +13,36 @@ from src.ImUtils import commonStackReader
 from utils.plots import *
 from utils.segment3D  import *
 
+
+def correct_cell_bottom(mask, full_tile_size=958):
+        
+        # determine zero-level
+        z0_ref   = estimate_cell_bottom_reference(mask)
+        
+        # splitting stack in tiles
+        tiles = split_tiles(mask) 
+
+        tile_dims    = np.shape(tiles)
+        mean_tile_z0 = np.zeros([tile_dims[3], tile_dims[4]])
+        weight_tile  = np.zeros([tile_dims[0], tile_dims[1]])
+
+        # computing mean tile for fitting plane to cell bottom and weights to scale adjustment with amount of data below z0_ref
+        for iy in range(4):
+            for ix in range(4):
+                mean_tile_z0 += tiles[iy, ix, z0_ref-1] / 16
+                weight_tile[iy, ix] = np.sum(tiles[iy, ix, z0_ref-2:z0_ref]) / (tile_dims[3] * tile_dims[4])
+
+        
+        # combine z0_plane of each tile to one image
+        z0_plane = fit_plane(mean_tile_z0, [full_tile_size, full_tile_size])
+        z0_image = combine_tiles(z0_plane, weight_tile)
+
+        new_mask = update_cell_mask(mask, z0_ref, z0_image)
+
+        return new_mask
+
+
+
 parser = argparse.ArgumentParser(description='Compute cell properties from segmented data')
 parser.add_argument('dir',             type=str, help="path to main dir")
 parser.add_argument('-m', '--method',  type=str, help="method for computing heights. 'sum' or 'diff'", default='sum')
@@ -49,8 +79,6 @@ for file in path.glob("*_HT3D_0_mask.tiff"):
 # loop through all positions in folder
 assert len(positions) > 0
 for pos in positions:
-    h_counts = []
-    n_counts = []
 
     print(f"\nComputing histogram for {pos} ...")
     for file in path.glob(f"{pos}*"):
@@ -60,6 +88,7 @@ for pos in positions:
         # load stacks
         stack = commonStackReader(stack_name)
         mask  = commonStackReader(file)
+        mask  = correct_cell_bottom(mask) 
 
         # compute and save height
         im_heights = compute_height(mask, method=args.method)

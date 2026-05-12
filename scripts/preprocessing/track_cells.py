a76238829e36e2bd13c6636e68ca2d3010c62ff1
siljabl <siljabl@github.com>
2026-05-12 17:37:21 +0200

    first commit

---
+++ scripts/preprocessing/track_cells.py
@@ -0,0 +1,130 @@
+
+"""
+Author S. B. Låstad
+
+Track cells with trackpy, using both position and area as tracking parameters
+"""
+
+import sys
+import json
+import argparse
+import numpy as np
+import pandas as pd
+import trackpy as tp
+
+from pathlib  import Path
+from datetime import datetime
+from skimage.measure import regionprops
+
+sys.path.append("scripts/utils/")
+from file_handling import *
+from segmentation_functions import *
+from microscope_features    import Holomonitor, Tomocube
+from data_class import SegmentationData
+
+
+# Paths
+config_path    = "configs/"
+height_path    = "../../../../hdd_data/silja/Monolayers/height_fields/"
+ri_path        = "../../../../hdd_data/silja/Monolayers/ri_fields/"
+label_path     = "../../../../hdd_data/silja/Monolayers/cell_labels/"
+cell_path      = "../../../../hdd_data/silja/Monolayers/cell_features/"
+
+
+parser = argparse.ArgumentParser(description="Usage: python track_cells.py dir microscope")
+parser.add_argument("path",     type=str,  help="Path to data folder. Typically '../../../../hdd_data/silja/Monolayers/height_fields/<dataset>/'")
+parser.add_argument("microscope",   type=str, help="Microscope that where used for data. 'holomonitor' or 'tomocube'.")
+args = parser.parse_args()
+
+
+
+### SET MICROSCOPE ###
+assert args.microscope == 'holomonitor' or args.microscope == 'tomocube', "Error: do not recognize microscope."
+
+if args.microscope == "holomonitor": microscope = Holomonitor()
+elif args.microscope == "tomocube":  microscope = Tomocube()
+
+
+
+### LOAD DATA ###
+dataset   = Path(args.path).stem
+config    = json.load(open(f"{config_path}/{dataset}.json"))
+im_areas  = load_label_images(f"{label_path}/{dataset}/corrected")
+im_height = import_stack(f"{height_path}/{dataset}/", config, microscope)
+
+
+# Compute region props of cells in all frames
+cellprops = [regionprops(im_areas[i], im_height[i]) for i in range(len(im_areas))]
+
+
+# Prepare dataframe for tracking
+Fcells = np.concatenate([[frame for cell in cellprops[frame]] for frame in range(len(cellprops))])
+
+Acells       = np.concatenate([[cell.area              for cell in cells] for cells in cellprops])
+hcells       = np.concatenate([[cell.mean_intensity    for cell in cells] for cells in cellprops])
+Lcells       = np.concatenate([[cell.label             for cell in cells] for cells in cellprops])
+amajor       = np.concatenate([[cell.axis_major_length for cell in cells] for cells in cellprops])
+aminor       = np.concatenate([[cell.axis_minor_length for cell in cells] for cells in cellprops])
+orientation  = np.concatenate([[cell.orientation       for cell in cells] for cells in cellprops])
+eccentricity = np.concatenate([[cell.eccentricity      for cell in cells] for cells in cellprops])
+pos_cells    = np.concatenate([[cell.centroid_weighted for cell in cells] for cells in cellprops])
+
+cells_df = pd.DataFrame({'x': pos_cells.T[1],
+                         'y': pos_cells.T[0],
+                         'area': Acells / microscope.A_scale,
+                         'hmean': hcells,
+                         'label': Lcells, 
+                         'a_max': amajor,
+                         'a_min': aminor,
+                         'theta': orientation, 
+                         'ecc': eccentricity,
+                         'frame': Fcells})
+
+# Add RI for Tomocube
+if microscope.name == "tomocube":
+
+    n_mean_arr = []
+    for i in range(len(im_areas)):
+        ri_field    = imageio.v2.imread(f"{height_path}/{dataset}/MDCK-li_refractive_index_{i}.tiff") * microscope.ri_conversion
+        n_cellprops = regionprops(im_areas[i], ri_field)
+
+        n_mean_arr.append([cell.mean_intensity for cell in n_cellprops])
+
+    cells_df['nmean'] = np.concatenate(n_mean_arr)
+
+
+### Track cells ###
+tracks = tp.link(cells_df, search_range=microscope.search_range, 
+                           memory=microscope.memory, 
+                           pos_columns=['x', 'y', 'hmean', 'area']);
+
+tracks = tp.filter_stubs(tracks, threshold=microscope.threshold);
+
+# Keep only frames where all cells can have tracks longer than threshold
+fmin = microscope.threshold - 1
+fmax = np.max(tracks.frame) + 1 - fmin
+tracks = tracks[(tracks.frame >= fmin) * (tracks.frame < fmax)]
+tracks.area *= microscope.A_scale
+
+
+# save as pickle
+data_obj = SegmentationData(f"{cell_path}/{dataset}_cells.p")
+data_obj.transform_df_to_ma(tracks, microscope.pix_to_um)
+data_obj.save(f"{cell_path}/{dataset}_cells.p")
+
+
+# Create new im_areas with only surviving cells
+im_areas_tracked = np.copy(im_areas)[fmin:-fmin]
+
+for i in range(len(im_areas_tracked)):
+    labels = tracks[tracks.frame==fmin + i].label.values
+    exclude = np.setdiff1d(np.unique(im_areas[i]), labels)
+
+    for cell in exclude:
+        mask = im_areas_tracked[i] == cell
+        im_areas_tracked[i][mask] = 0
+
+# Save im_areas
+Path(f"{label_path}{dataset}/tracked/").mkdir(parents=True, exist_ok=True)
+save_id_images(im_areas_tracked, tracks, f"{label_path}/{dataset}/tracked", fmin=fmin)
+

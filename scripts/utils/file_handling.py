26afebac423fb516ba65a880591c95f6fd2cab99
siljabl <siljabl@github.com>
2026-05-12 18:05:50 +0200

    added load_stack

--- scripts/utils/file_handling.py
+++ scripts/utils/file_handling.py
@@ -4,10 +4,12 @@ import imageio
 import numpy as np
 
 from pathlib import Path
-from microscope_features import get_voxel_size_35mm
+from microscope_features import get_voxel_size_35mm, Holomonitor, Tomocube
 
 height_path = "../../../../hdd_data/silja/Monolayers/height_fields/"
 ri_path     = "../../../../hdd_data/silja/Monolayers/ri_fields/"
+
+
 
 def load_set_of_frames(dataset, frame, microscope):
 
@@ -21,18 +23,36 @@ def load_set_of_frames(dataset, frame, microscope):
 
     return h_im, n_im
 
+    
+
+def load_stack(dir, config, param="h", preprocessing_step="final"):
+
+    f_min = config[preprocessing_step]['fmin']
+    f_max = config[preprocessing_step]['fmax']
+    
+    if config['microscope'] == "holomonitor": 
+        microscope = Holomonitor()
+        stack = import_holomonitor_stack(dir, f_min, f_max)
+
+    elif config['microscope'] == "tomocube":  
+        microscope = Tomocube()
+        stack = import_tomocube_stack(dir, f_min, f_max, param)
+
+    if param == "height" or param == "h":
+        stack = stack * microscope.h_to_um
+    elif param == "n" or param == "ri":
+        stack = stack * microscope.ri_conversion
+   
+    return stack
 
 
 
-def import_holomonitor_stack(dir, h_scaling=4 / 3 * (1 / 100), f_min=1, f_max=180):
+def import_holomonitor_stack(dir, f_min=1, f_max=180):
     """
     Import tiff files to one list
 
     Parameters:
     - dir: directory of tiff files.
-    - h_scaling: converts height to µm. Factor 4/3 comes from updating mean ri from 1.38 to 1.37.
-    - f_min: first frame
-    - f_max: last frame
     """
 
     # mask that set area outside cells to zero
@@ -55,160 +75,149 @@ def import_holomonitor_stack(dir, h_scaling=4 / 3 * (1 / 100), f_min=1, f_max=18
 
         stack.append(frame * mask)
 
-    stack = np.array(stack) * h_scaling
-
-    return stack
+    return np.array(stack)
 
 
 
-def import_tomocube_stack(dir, h_scaling, f_min=0, f_max=40, n_cell=1.38):
+def import_tomocube_stack(dir, f_min=0, f_max=40, param="h"):
     """
     Import tiff files to one list
 
     Parameters:
-    - dir: directory of tiff files.
-    - h_scaling: converts height to µm
+    - dir: directory of tiff files
     - f_min: first frame
     - f_max: last frame
-    - n_cell: mean refractive index of cells
     """
+    if param == "height" or param == "h":
+        fname = "MDCK-li_height"
+    elif param == "n" or param == "ri":
+        fname = "MDCK-li_refractive_index"
 
-    n_stack = []
-    h_stack = []
+    stack = []
     for f in range(f_min, f_max+1):
-        h_frame = imageio.v2.imread(f"{dir}/MDCK-li_height_{f}.tiff")
-        n_frame = imageio.v2.imread(f"{dir}/MDCK-li_refractive_index_{f}.tiff")
+        frame = imageio.v2.imread(f"{dir}/{fname}_{f}.tiff")
+        stack.append(frame)
 
-        h_stack.append(h_frame)
-        n_stack.append(n_frame)
-
-    # scale data
-    h_stack = np.array(h_stack) * h_scaling
-    n_stack = np.array(n_stack) / 10_000
-    #n_mean  = np.mean(n_stack[h_stack > 0])
-    #n_stack = n_stack * n_cell / n_mean
-
-    return n_stack, h_stack
+    return np.array(stack)
 
 
 
-def import_txt_with_NaN(input_file, header_rows=3):
-    with open(input_file, 'r') as file:
-
-        # Skip the first three lines
-        for _ in range(header_rows):
-            next(file)
-        
-        # Read the remaining lines and replace 'NaN'
-        output_file = []
-
-        # Process the remaining rows
-        for line in file:
-            # Split the line into values (assuming a whitespace delimiter)
-            values = line.strip().split(",")
-
-            # Replace "NaN" with the specified replacement value and convert to float
-            processed_row = [
-                float(value) if value != "NaN" else 9999.0
-                for value in values
-            ]
-            output_file.append(processed_row)
+# def import_stack(dir, config, microscope, return_n=False):
     
-    output_file = np.array(output_file)
-    output_file = np.ma.array(output_file, mask=output_file==9999.0)
+#     f_min = config['detection']['fmin']
+#     f_max = config['detection']['fmax']
 
-    return output_file
+#     if microscope.name == 'holomonitor':
+#         h_stack = import_holomonitor_stack(dir, f_min=f_min, f_max=f_max)
+
+#     elif microscope.name == 'tomocube':
+#         pix_to_um = get_voxel_size_35mm()
+#         n_stack, h_stack = import_tomocube_stack(dir, h_scaling=pix_to_um[0]/1000, f_min=f_min, f_max=f_max)
+
+#     else:
+#         print("microscope not recognized: ", microscope.name)
+#         h_stack = 0
+        
+#     if return_n:
+#         return n_stack
+
+#     else:
+#         return h_stack
 
 
 
 
-def import_stack(dir, config, microscope, return_n=False):
+# def import_txt_with_NaN(input_file, header_rows=3):
+#     with open(input_file, 'r') as file:
+
+#         # Skip the first three lines
+#         for _ in range(header_rows):
+#             next(file)
+        
+#         # Read the remaining lines and replace 'NaN'
+#         output_file = []
+
+#         # Process the remaining rows
+#         for line in file:
+#             # Split the line into values (assuming a whitespace delimiter)
+#             values = line.strip().split(",")
+
+#             # Replace "NaN" with the specified replacement value and convert to float
+#             processed_row = [
+#                 float(value) if value != "NaN" else 9999.0
+#                 for value in values
+#             ]
+#             output_file.append(processed_row)
     
-    f_min = config['segmentation']['fmin']
-    f_max = config['segmentation']['fmax']
+#     output_file = np.array(output_file)
+#     output_file = np.ma.array(output_file, mask=output_file==9999.0)
 
-    if microscope == 'holomonitor':
-        h_stack = import_holomonitor_stack(dir, f_min=f_min, f_max=f_max)
+#     return output_file
 
-    elif microscope == 'tomocube':
-        pix_to_um = get_voxel_size_35mm()
-        n_stack, h_stack = import_tomocube_stack(dir, h_scaling=pix_to_um[0]/1000, f_min=f_min, f_max=f_max)
 
-    else:
-        print("microscope not recognized: ", microscope)
-        h_stack = 0
+
+# def import_PIV_velocity(path, config, _shape):
+
+#     # import PIV velocity field
+#     data_PIV = import_txt_with_NaN(f"{path}/PIVlab_0001.txt", header_rows=3)
+#     x = np.ma.array(data_PIV[:, 0], dtype=int)
+#     y = np.ma.array(data_PIV[:, 1], dtype=int)
+
+#     # import heights to mask image
+#     dataset = Path(path).stem
+#     stack = import_stack(f"data/experimental/raw/{dataset}", config)
+
+#     # transform PIV position to matrix entries
+#     dx   = y[1] - y[0]
+#     x0   = y[0] - dx
+#     xmax = int((np.ma.max(y) - x0) / dx) + 1
+
+#     x_tmp = np.ma.array((x - x0) / dx, dtype=int)
+#     y_tmp = np.ma.array((y - x0) / dx, dtype=int)
+
+#     position = np.ma.zeros((2, _shape[0], xmax, xmax), dtype=np.float64)
+#     velocity = np.ma.zeros((2, _shape[0], xmax, xmax), dtype=np.float64)
+
+#     h_mask   = np.ma.zeros((_shape[0], xmax, xmax), dtype=np.float64)
+
+#     i = 0
+#     for i in range(1, _shape[0]):
         
-    if return_n:
-        return n_stack
+#         # Load data
+#         data_PIV = import_txt_with_NaN(f"{path}/PIVlab_{i:04d}.txt", header_rows=3)
 
-    else:
-        return h_stack
+#         # Extract values
+#         u = np.ma.array(data_PIV[:, 2], dtype=np.float64)
+#         v = np.ma.array(data_PIV[:, 3], dtype=np.float64)
 
-
-
-
-def import_PIV_velocity(path, config, _shape):
-
-    # import PIV velocity field
-    data_PIV = import_txt_with_NaN(f"{path}/PIVlab_0001.txt", header_rows=3)
-    x = np.ma.array(data_PIV[:, 0], dtype=int)
-    y = np.ma.array(data_PIV[:, 1], dtype=int)
-
-    # import heights to mask image
-    dataset = Path(path).stem
-    stack = import_stack(f"data/experimental/raw/{dataset}", config)
-
-    # transform PIV position to matrix entries
-    dx   = y[1] - y[0]
-    x0   = y[0] - dx
-    xmax = int((np.ma.max(y) - x0) / dx) + 1
-
-    x_tmp = np.ma.array((x - x0) / dx, dtype=int)
-    y_tmp = np.ma.array((y - x0) / dx, dtype=int)
-
-    position = np.ma.zeros((2, _shape[0], xmax, xmax), dtype=np.float64)
-    velocity = np.ma.zeros((2, _shape[0], xmax, xmax), dtype=np.float64)
-
-    h_mask   = np.ma.zeros((_shape[0], xmax, xmax), dtype=np.float64)
-
-    i = 0
-    for i in range(1, _shape[0]):
+#         # masking didn't work properly, so probably mean is affected by outside data points
+#         position[:, i-1, x_tmp, y_tmp] = [x, y]
+#         velocity[:, i-1, x_tmp, y_tmp] = [u, v]
         
-        # Load data
-        data_PIV = import_txt_with_NaN(f"{path}/PIVlab_{i:04d}.txt", header_rows=3)
+#         h_mask[i-1, x_tmp, y_tmp] = stack[i-1, x, y]
 
-        # Extract values
-        u = np.ma.array(data_PIV[:, 2], dtype=np.float64)
-        v = np.ma.array(data_PIV[:, 3], dtype=np.float64)
-
-        # masking didn't work properly, so probably mean is affected by outside data points
-        position[:, i-1, x_tmp, y_tmp] = [x, y]
-        velocity[:, i-1, x_tmp, y_tmp] = [u, v]
-        
-        h_mask[i-1, x_tmp, y_tmp] = stack[i-1, x, y]
-
-    velocity[0][h_mask <= 0] = 0
-    velocity[1][h_mask <= 0] = 0
+#     velocity[0][h_mask <= 0] = 0
+#     velocity[1][h_mask <= 0] = 0
 
 
-    return position, velocity
+#     return position, velocity
 
 
 
 
 
-def import_PIV_frame(path, frame):
+# def import_PIV_frame(path, frame):
 
-    # import PIV velocity field
-    data_PIV = import_txt_with_NaN(f"{path}/PIVlab_{frame+1:04d}.txt", header_rows=3)
+#     # import PIV velocity field
+#     data_PIV = import_txt_with_NaN(f"{path}/PIVlab_{frame+1:04d}.txt", header_rows=3)
 
-    # Extract values
-    x = np.ma.array(data_PIV[:, 0], dtype=int)
-    y = np.ma.array(data_PIV[:, 1], dtype=int)
-    u = np.ma.array(data_PIV[:, 2], dtype=np.float64)
-    v = np.ma.array(data_PIV[:, 3], dtype=np.float64)
+#     # Extract values
+#     x = np.ma.array(data_PIV[:, 0], dtype=int)
+#     y = np.ma.array(data_PIV[:, 1], dtype=int)
+#     u = np.ma.array(data_PIV[:, 2], dtype=np.float64)
+#     v = np.ma.array(data_PIV[:, 3], dtype=np.float64)
 
-    return [x,y], [u,v]
+#     return [x,y], [u,v]
 
 
 
